192e8457c486ea65ee077b8020865e33ed18c4d2
everysick <s.wakeup31@gmail.com>
2017-08-03 16:56:38 +0900

    Solve s8

--- exercises/lifetime-and-function/src/main.rs
+++ exercises/lifetime-and-function/src/main.rs
@@ -10,28 +10,25 @@ Goals:
 
 fn main() {
     points();
-    /* [1]
     area1();
-    */
-    /* [3]
     area2();
-    */
 }
 
 fn points() {
     let from = Point::new(10.0, 10.0);
     let diff = Point::new(5.0, 10.0);
     let to = from.add(&diff);
-    println!("({}, {})を({}, {})動かした場所は({}, {})です",
-             from.x,
-             from.y,
-             diff.x,
-             diff.y,
-             to.x,
-             to.y);
+    println!(
+        "({}, {})を({}, {})動かした場所は({}, {})です",
+        from.x,
+        from.y,
+        diff.x,
+        diff.y,
+        to.x,
+        to.y
+    );
 }
 
-/* [2]
 fn area1() {
     let top = Point::new(10.0, 10.0);
     let diff = Point::new(5.0, 10.0);
@@ -39,9 +36,7 @@ fn area1() {
     let rect = Rect::new(&top, &bottom);
     println!("area = {}", rect.area());
 }
-*/
 
-/* [4]
 fn area2() {
     let top = Point::new(10.0, 10.0);
     let rect;
@@ -52,7 +47,7 @@ fn area2() {
     }
     println!("area = {}", rect.area());
 }
-*/
+
 struct Point {
     x: f32,
     y: f32,
@@ -69,14 +64,13 @@ impl Point {
     }
 }
 
-/* [2]
-struct Rect {
+struct Rect<'a, 'b> {
     top_left: &'a Point,
     bottom_right: &'b Point,
 }
 
-impl Rect {
-    fn new(top_left: &'a Point, bottom_right: &'b Point) -> Rect {
+impl<'a, 'b> Rect<'a, 'b> {
+    fn new(top_left: &'a Point, bottom_right: &'b Point) -> Rect<'a, 'b> {
         Rect {
             top_left: top_left,
             bottom_right: bottom_right,
@@ -88,4 +82,3 @@ impl Rect {
         dx * dy
     }
 }
-*/
